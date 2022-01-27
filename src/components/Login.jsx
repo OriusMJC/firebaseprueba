06601d80bdebdcaa5c1b97629f0290e8fc7af33a
OriusMJC <matiasjesuscontreras12@hotmail.com>
2022-01-27 09:58:57 -0300

    pequeño avance

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,70 @@
+import React,{useState} from 'react'
+import {auth, createUserWithEmailAndPassword} from '../firebaseconfig'
+
 export default function Login(){
+
+    const [email,setEmail] = useState('')
+    const [pass,setPass] = useState('')
+    const [msgError,setmsgError] = useState(null)
+
+    const registrarUsuario = (e)=>{
+        e.preventDefault()
+        createUserWithEmailAndPassword(auth,email,pass)
+        
+        .then( r => alert('Usuario registrado'))
+        setEmail('')
+        setPass('')
+        .catch((e) =>{
+            if (e.code == 'auth/invalid-email'){
+                setmsgError('Wrong format for Email address')
+            }
+            if (e.code == 'auth/weak-password'){
+                setmsgError('The Password should be at least 6 characters')
+            }
+        })
+    }
+
     return(
         <div className="row mt-5">
             <div className="col"></div>
             <div className="col">
-                <form className="form-group">
-                    <input placeholder="Introduce el Email" type='email' className="form-control"></input>
-                    <input placeholder="Introduce el Email" type='password' className="form-control mt-2"></input>
+                <form onSubmit={registrarUsuario} className="form-group">
+                    <input
+                        onChange={(e)=>{setEmail(e.target.value)}} 
+                        placeholder="Introduce el Email" 
+                        type='email' 
+                        className="form-control"
+                    />
+                    <input
+                        onChange={(e)=>{setPass(e.target.value)}} 
+                        placeholder="Introduce el Email" 
+                        type='password' 
+                        className="form-control mt-2"/
+                    >
                     <div className="d-grid gap-2">
-                        <input value="Iniciar Sesion" type='submit' className="btn btn-primary btn-block mt-2"></input>
-                        <input value="Registrar Ususario" type='submit' className="btn btn-dark btn-block mt-auto"></input>
+                        <input 
+                            value="Iniciar Sesion" 
+                            type='submit'
+                            className="btn btn-primary btn-block mt-2"
+                        />
+                        <input 
+                            value="Registrar Ususario" 
+                            type='submit' 
+                            className="btn btn-dark btn-block mt-auto"
+                        />
                     </div>
                 </form>
+                <div>
+                    {
+                        msgError !== null ? 
+                        (
+                            <div className='alert alert-danger'>{msgError}</div>
+                        ):
+                        (
+                            <span/>
+                        )
+                    }
+                </div>
             </div>
             <div className="col"></div>
         </div>
--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,18 +1,23 @@
-import firebase from 'firebase'
-import 'firebase/auth'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD0K7eL03zhZclycvutV4Nc_K6lOvQRkRE",
-    authDomain: "fir-tuto-d6b38.firebaseapp.com",
-    projectId: "fir-tuto-d6b38",
-    storageBucket: "fir-tuto-d6b38.appspot.com",
-    messagingSenderId: "595085462503",
-    appId: "1:595085462503:web:74e6bce729f9b0ea92cb42",
-    measurementId: "G-D02N1DWY88"
-  };
-  
-  // Initialize Firebase
-  const fire = initializeApp(firebaseConfig);
-  const auth = fire.auth()
+  apiKey: "AIzaSyD0K7eL03zhZclycvutV4Nc_K6lOvQRkRE",
+  authDomain: "fir-tuto-d6b38.firebaseapp.com",
+  projectId: "fir-tuto-d6b38",
+  storageBucket: "fir-tuto-d6b38.appspot.com",
+  messagingSenderId: "595085462503",
+  appId: "1:595085462503:web:74e6bce729f9b0ea92cb42",
+  measurementId: "G-D02N1DWY88"
+};
 
-export {auth}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+export {auth, createUserWithEmailAndPassword}
